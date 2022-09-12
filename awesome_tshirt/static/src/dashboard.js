1a4847331b3c9d70283b4e1033e974ce4abce446
Dardenne Florent (dafl) <dafl@odoo.com>
2022-09-12 09:19:50 +0200

    Translation, Background and others

--- awesome_tshirt/static/src/dashboard.js
+++ awesome_tshirt/static/src/dashboard.js
@@ -8,6 +8,7 @@ import { useService } from "@web/core/utils/hooks";
 import { Domain } from "@web/core/domain";
 import { Card } from "./card/card";
 import { PieChart } from "./pie_chart/pie_chart";
+import { sprintf } from "@web/core/utils/strings";
 
 class AwesomeDashboard extends Component {
 	setup() {
@@ -25,11 +26,13 @@ class AwesomeDashboard extends Component {
         this.tshirtService = useService("tshirtService");
 
         this.keyToString = {
-            average_quantity: "Average amount of t-shirt by order this month",
-            average_time: "Average time for an order to go from 'new' to 'sent' or 'cancelled'",
-            nb_cancelled_orders: "Number of cancelled orders this month",
-            nb_new_orders: "Number of new orders this month",
-            total_amount: "Total amount of new orders this month",
+            average_quantity: this.env._t("Average amount of t-shirt by order this month"),
+            average_time: this.env._t(
+                "Average time for an order to go from 'new' to 'sent' or 'cancelled'"
+            ),
+            nb_cancelled_orders: this.env._t("Number of cancelled orders this month"),
+            nb_new_orders: this.env._t("Number of new orders this month"),
+            total_amount: this.env._t("Total amount of new orders this month"),
         };
 
         onWillStart(async () => {
@@ -54,6 +57,12 @@ class AwesomeDashboard extends Component {
         });
     }
 
+    openLast7DaysOrders() {
+        const domain =
+            "[('create_date','>=', (context_today() - datetime.timedelta(days=7)).strftime('%Y-%m-%d'))]";
+        this.openOrders(this.env._t("Last 7 days orders", domain));
+    }
+
 	openNewOrders() {
 		const domain =
             "[('create_date','>=', (context_today() - datetime.timedelta(days=7)).strftime('%Y-%m-%d'))]";
@@ -63,8 +72,15 @@ class AwesomeDashboard extends Component {
 	openCancelledOrders() {
 		const domain =
             "[('create_date','>=', (context_today() - datetime.timedelta(days=7)).strftime('%Y-%m-%d')), ('state','=', 'cancelled')]";
-        this.openOrders("Cancelled Orders", domain);
-	}
+
+        this.openOrders(this.env._t("Last 7 days cancelled orders"), domain);
+    }
+
+    openFilteredBySizeOrders(size) {
+        const title = sprintf(this.env._t("Filtered orders by %s size"), size);
+        const domain = `[('size','=', '${size}')]`;
+        this.openOrders(title, domain);
+    }
 }
 
 AwesomeDashboard.components = { Layout, Card, PieChart };
