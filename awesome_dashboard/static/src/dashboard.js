4df3073011be4ecb5121f958edd6772b4885f13d
Géry Debongnie <ged@odoo.com>
2023-05-10 17:27:36 +0200

    [Solution] 2.3 Add a DashboardItem

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -4,11 +4,12 @@ import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
 import { useService } from "@web/core/utils/hooks";
+import { DashboardItem } from "./dashboard_item/dashboard_item";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.clientaction";
     static props = ['*'];
-    static components = { Layout };
+    static components = { Layout, DashboardItem };
 
     setup() {
         this.action = useService("action");
