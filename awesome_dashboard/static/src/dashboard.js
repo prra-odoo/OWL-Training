6956c4a175d4caa12f45750e582c575259c68ac8
Géry Debongnie <ged@odoo.com>
2023-05-10 16:56:53 +0200

    [Solution] 2.1 A new Layout

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -2,10 +2,18 @@
 
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.clientaction";
     static props = ['*'];
+    static components = { Layout };
+
+    setup() {
+        this.display = {
+            controlPanel: {},
+        };
+    }
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
