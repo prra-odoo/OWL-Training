muhrizky/tutorials-owl

[Solution] 2.5 Cache network calls, create a service

awesome_dashboard/static/src/dashboard.js
@@ -12,12 +12,12 @@ class AwesomeDashboard extends Component {
 
     setup() {
         this.action = useService("action");
-        this.rpc = useService("rpc");
+        this.dashboardService = useService("awesome_dashboard.dashboard");
         this.display = {
             controlPanel: { "top-right": false, "bottom-right": false },
         };
         onWillStart(async () => {
-            this.statistics = await this.rpc("/awesome_dashboard/statistics");
+            this.statistics = await this.dashboardService.loadStatistics();
         });
     }
 

prra-odoo/OWL-Training

awesome_dashboard/static/src/dashboard.js
@@ -13,12 +13,12 @@ class AwesomeDashboard extends Component {
 
     setup() {
         this.action = useService("action");
-        this.rpc = useService("rpc");
+        this.statistics = useService("awesome_dashboard.statistics");
         this.display = {
             controlPanel: {},
         };
         onWillStart(async () => {
-            this.statistics = await this.rpc("/awesome_dashboard/statistics");
+            this.statistics = await this.statistics.loadStatistics();
         });
     }
 