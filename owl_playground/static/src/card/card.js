0a7f0c00523620b80932b3bb844a1fe767ec03ae
Nisarg Patel <nipa@odoo.com>
2023-06-28 17:49:00 +0530

    OWL training

---
+++ owl_playground/static/src/card/card.js
@@ -0,0 +1,17 @@
+/** @odoo-module **/
+
+import { Component } from "@odoo/owl";
+
+export class Card extends Component {
+  static template = "owl_playground.card";
+
+  static props = {
+    slots: {
+      type: Object,
+      shape: {
+        default: Object,
+        title: { type: Object, optional: true },
+      },
+    },
+  };
+}
--- owl_playground/static/src/playground.js
+++ owl_playground/static/src/playground.js
@@ -1,7 +1,16 @@
 /** @odoo-module **/
-
-import { Component } from "@odoo/owl";
+import { Card } from "./card/card";
+import { Counter } from "./Counter/counter";
+import { TodoList } from "./todo_list/todo_list";
+import { Todo } from "./todo/todo";
+import { Component, useState } from "@odoo/owl";
 
 export class Playground extends Component {
-    static template = "owl_playground.playground";
+  static template = "owl_playground.playground";
+
+  setup(){
+    
+  }
+
+  static components = { Counter, TodoList, Card, Todo };
 }
