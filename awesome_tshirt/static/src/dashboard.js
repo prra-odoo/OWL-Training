ba8a99ec102bb33be4b921eebca03b18a282edf6
kusi-odoo <kusi@odoo.com>
2023-06-29 10:15:46 +0530

    js owl training code

--- awesome_tshirt/static/src/dashboard.js
+++ awesome_tshirt/static/src/dashboard.js
@@ -1,11 +1,100 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout"
+import { Card } from "./card/card";
+import { getDefaultConfig } from "@web/views/view";
+import { Component, useSubEnv,onWillStart} from "@odoo/owl";
+import { Domain } from "@web/core/domain";
+import { useService } from "@web/core/utils/hooks";
+import { PieChart } from "./pie_chart/pie_chart";
+import { sprintf } from "@web/core/utils/strings";
+ 
+class AwesomeDashboard extends Component {
+    setup() {
+                useSubEnv({
+                    config: {
+                        ...getDefaultConfig(),
+                        ...this.env.config,
+                    },
 
-class AwesomeDashboard extends Component {}
+                });
 
-AwesomeDashboard.components = {};
+                this.action = useService("action");
+            
+                // const myService = {
+                //     dependencies: ["notification"],
+                //     start(env, { notification }) {
+                //         let counter = 1;
+                //         setInterval(() => {
+                //             notification.add(`Tick Tock ${counter++}`);
+                //         }, 5000);
+                //     }
+                // };
+
+                // const effectService = useService("effect");
+                // effectService.add({
+                //      type: "rainbow_man",
+                //      message: "Boom! Team record for the past 30 days.",
+                //  });
+                
+                this.tshirtService = useService("tshirtService");
+
+                this.keyToString = {
+                    average_quantity: this.env._t("Average amount of t-shirt by order this month"),
+                    average_time: this.env._t("Average time for an order to go from 'new' to 'sent' or 'cancelled'"),
+                    nb_cancelled_orders: this.env._t("Number of cancelled orders this month"),
+                    nb_new_orders: this.env._t("Number of new orders this month"),
+                    total_amount: this.env._t("Total amount of new orders this month"),
+                };
+
+                onWillStart(async ()=>{
+                    this.statistics = await this.tshirtService.loadStatistics();
+                });
+
+                
+            }
+
+            openCustomerView(){
+               this.action.doAction("base.action_partner_form");
+            }
+
+            openOrders(title,domain){
+                this.action.doAction({
+                    type: "ir.actions.act_window",
+                    name: title,
+                    res_model: "awesome_tshirt.order",
+                    domain: new Domain(domain).toList(),
+                    views: [ [false, "list"], [false, "form"], ],
+                });
+            }
+
+            openLast7DaysOrderes() {
+                const domain =
+                    "[('create_date','>=', (context_today() - datetime.timedelta(days=7)).strftime('%Y-%m-%d'))]";
+                this.openOrders(this.env._t("Last 7 days orders"), domain);
+            }
+        
+            openLast7DaysCancelledOrders() {
+                const domain =
+                    "[('create_date','>=', (context_today() - datetime.timedelta(days=7)).strftime('%Y-%m-%d')), ('state','=', 'cancelled')]";
+                this.openOrders(this.env._t("Last 7 days cancelled orders"), domain);
+            }
+
+            openFilteredBySizeOrders(size) {
+                const title = sprintf(this.env._t("Filtered orders by %s size"), size);
+                const domain = `[('size','=', '${size}')]`;
+                this.openOrders(title, domain);
+            }    
+            
+}
+
+AwesomeDashboard.components = { Layout, Card, PieChart };
 AwesomeDashboard.template = "awesome_tshirt.clientaction";
 
 registry.category("actions").add("awesome_tshirt.dashboard", AwesomeDashboard);
+// registry.category("services").add("myService", myService);
+
+
+
+
--- owl_playground/static/src/playground.js
+++ owl_playground/static/src/playground.js
@@ -1,7 +1,34 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Card } from "./card/card";
+import { Counter } from "./counter/counter";
+import { TodoList } from "./todo_list/todo_list";
+import { Component, onWillRender,onWillStart,onMounted,onWillUpdateProps,onWillPatch,onPatched,onRendered,onWillUnmount,onWillDestroy,onError} from "@odoo/owl";
 
 export class Playground extends Component {
+    
     static template = "owl_playground.playground";
+    static components = {Counter,TodoList,Card};
+
+
+   setup(){
+    onWillRender(()=>{console.log('will render')});
+    onWillStart(()=>{console.log('Will start')});
+    onMounted(()=>{console.log('will mount')});
+    onWillUpdateProps(()=>{console.log('updated props')});
+    onWillPatch(()=>(console.log('Will patch')));
+    onPatched(()=>{console.log('pached')});
+    onRendered(()=>{console.log('rendered')});
+    onWillUnmount(()=>{console.log('will unmount')});
+    onWillDestroy(()=>{console.log('will destroy')});
+    onError((ev)=>{console.log(ev)});
+}
+
+   show(){
+    this.state.display = true;
+   }
+   hide(){
+    this.state.display = false;
+   }
+
 }
